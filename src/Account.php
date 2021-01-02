67afae2a9548e77981a055224deff82d5788194a
Özgür KARALAR <zgr@zgrkaralar.com>
2021-01-02 03:24:36 +0300

    Index function for Account class

--- src/Account.php
+++ src/Account.php
@@ -3,6 +3,15 @@ namespace Parasut;
 
 class Account extends Base
 {
+    public function index($pagenumber=1,$size=15,$data = [])
+    {
+        return $this->client->request(
+            'contacts?page%5Bnumber%5D='.$pagenumber.'&page%5Bsize%5D='.$size,
+            $data,
+            'GET'
+        );
+    }
+    
     public function create($data)
     {
         return $this->client->request(
